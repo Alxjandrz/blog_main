602c20e111407943c89f8a3fcd7c01ae1d97467d
Alxjandrz <omarubio2404@gmail.com>
2026-03-24 17:17:24 -0700

    Actualizacion de archivos python

--- blog/models.py
+++ blog/models.py
@@ -44,3 +44,18 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.name
+    
+        def get_absolute_url(self):
+         return '/%s/%s/' % (self.category.slug, self.slug)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
